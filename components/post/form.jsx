a7f599b0dde573b54b466bca8bbee27f1ba721a4
Brit Beaty <britneybeaty@gmail.com>
2025-09-09 17:02:44 -0500

    fixed alignment of filter bar

--- components/post/form.jsx
+++ components/post/form.jsx
@@ -29,7 +29,7 @@ export default function PostForm({
 
   useEffect(() => {
     if (profileData?.postalCode) {
-      setPostalCode(profileData.postalCode);
+      setPostalCodne(profileData.postalCode);
     }
   }, [profileData]);
 
